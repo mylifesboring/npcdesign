69e81a8ef44ebbffc74e0caecea01d3253155893
zohaib khan <21331583@student.uwl.ac.uk>
2018-07-02 21:58:26 +0100

    added association lists

--- birdie.rkt
+++ birdie.rkt
@@ -1,5 +1,18 @@
 #lang racket
 
+(define descriptions '((1 "what type of movies do you like?")
+                       (2 "so u like horror movies?")
+                       (3 "want me to search for some horror movies for you?")
+                       (4 "want me to search for some non horror movies for you?")))
+
+(define decisiontable `((1 ((horror) 2) ((scary) 2)
+                        
+                        (2 ((yes) 3) ((yea) 3) ((yeah) 3) ((correct) 3) ((no) 1) ((not correct) 1)
+                      
+                        (3 ((yes) horror) ((yea) horror) ((yeah) horror) ((no) 4)
+            
+                        (4 ((yes) non-horror) ((no) quit)))))))
+                 
 (define (search initial-id)
 
     (let* ((input (read-line))
